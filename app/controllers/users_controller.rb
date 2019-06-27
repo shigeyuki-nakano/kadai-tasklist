13d50013f8d0457de4476195dbd21b408445f9c8
shigeyuki.nakano <thuyajino_in_the_groove@outlook.jp>
2019-06-27 01:53:56 +0000

    Only logged in user can look

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @tasks = @user.tasks.order(id: :desc).page(params[:page])
-    counts(@user)
   end  
   
   def new
